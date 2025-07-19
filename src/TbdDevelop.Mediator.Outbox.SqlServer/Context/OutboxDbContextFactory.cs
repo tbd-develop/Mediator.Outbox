6aeaa87b72b4dd5a8684389fa146ef042a173d67
Terry Burns-Dyson <terrybd@gmail.com>
2025-07-19 14:40:18 -0400

    chore: Include design time factory for self migration
    - Update to latest versions of nuget packages

---
+++ src/TbdDevelop.Mediator.Outbox.SqlServer/Context/OutboxDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TbdDevelop.Mediator.Outbox.SqlServer.Context;
+
+public class OutboxDbContextFactory : IDesignTimeDbContextFactory<OutboxDbContext>
+{
+    public OutboxDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OutboxDbContext>();
+
+        // Use a dummy connection string for design-time - migrations don't need a real database
+        optionsBuilder.UseSqlServer("Server=localhost;Database=DesignTimeOnly;Trusted_Connection=true;");
+
+        return new OutboxDbContext(optionsBuilder.Options);
+    }
+}
